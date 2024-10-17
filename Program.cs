9145f2c24f00e003cd0cdd4c5b08a8bc470b5464
lucascod3 <lukinha_matheus09@hotmail.com>
2024-10-17 01:12:24 -0300

    trabalho feito

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Criando um objeto Nokia
+        Nokia nokia = new Nokia(numero: "123456789", modelo: "Nokia 3310", imei: "123456789012345", memoria: 16);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine();
+
+        // Criando um objeto Iphone
+        Iphone iphone = new Iphone(numero: "987654321", modelo: "iPhone 12", imei: "987654321012345", memoria: 128);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
